a0d119531a277c0f37bc70aeb4a28d48cde42d29
denshou <132555762+denshou@users.noreply.github.com>
2023-10-13 00:30:59 +0900

    1

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,15 +1,15 @@
-// import CartPageContent from '../../components/CartPageContent/CartPageContent';
-// import PageSection from '../../components/PageSection';
-// import Main from '../../components/ui/Main/Main';
+import CartPageContent from '../../components/CartPageContent/CartPageContent';
+import PageSection from '../../components/PageSection';
+import Main from '../../components/ui/Main/Main';
 
-// const CartPage = () => {
-//   return (
-//     <Main>
-//       <PageSection titleText="장바구니">
-//         <CartPageContent />
-//       </PageSection>
-//     </Main>
-//   );
-// };
+const CartPage = () => {
+  return (
+    <Main>
+      <PageSection titleText="장바구니">
+        <CartPageContent />
+      </PageSection>
+    </Main>
+  );
+};
 
-// export default CartPage;
+export default CartPage;
